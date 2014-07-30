d469df2bf9d842b85f2835ec764c998198dfe5a5
Mikael Rekola <mikael.rekola@gmail.com>
2014-07-30 19:53:01 +0300

    add methods and fields, switch to doule

--- src/Context.h
+++ src/Context.h
@@ -3,26 +3,48 @@
 
 #include <string>
 
+#include "Color.h"
 #include "Style.h"
+#include "Font.h"
 
 namespace canvas {  
+  struct Size {
+    float width, height;
+  };
+
   class Context {
   public:
     Context() { }
     virtual ~Context() { }
-    
-    virtual void clearRect(float x, float y, float w, float h) = 0;
-    virtual void fillRect(float x, float y, float w, float h) = 0;
-    virtual void moveTo(float x, float y) = 0;
-    virtual void lineTo(float x, float y) = 0;
+
+    virtual void beginPath() = 0;
+    virtual void closePath() = 0;
+    virtual void clip() = 0;
+
+    virtual void arc(double x, double y, double r, double a0, double a1, bool t) = 0;    
+    virtual void clearRect(double x, double y, double w, double h) = 0;
+    virtual void fillRect(double x, double y, double w, double h) = 0;
+    virtual void moveTo(double x, double y) = 0;
+    virtual void lineTo(double x, double y) = 0;
     virtual void stroke() = 0;
     virtual void fill() = 0;
+    virtual void fillText(const std::string & text, double x, double y) = 0;
+    virtual Size measureText(const std::string & text) = 0;
 
-    float lineWidth;
+    virtual void drawImage(Context & other, double x, double y, double w, double h) = 0;
+
+    float lineWidth = 1.0f;
     Style fillStyle;
     Style strokeStyle;
-
+    float shadowBlur = 0.0f;
+    Color shadowColor;
+    float shadowOffsetX = 0.0f, shadowOffsetY = 0.0f;
+    float globalAlpha = 1.0f;
+    Font font;
+    
   private:
+    Context(const Context & other) { }
+    Context & operator=(const Context & other) { return *this; }
   };
 };
 
