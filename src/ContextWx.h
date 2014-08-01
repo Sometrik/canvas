3cc9492185df8fd4d0f1c5634f369e0738712384
Mikael Rekola <mikael.rekola@gmail.com>
2014-08-01 19:16:35 +0300

    add WxSurface to wxWidgets context

--- src/ContextWx.h
+++ src/ContextWx.h
@@ -4,43 +4,40 @@
 #include <iostream>
 
 namespace canvas {
-
+  class WxSurface : public Surface {
+  public:
+    WxSurface(unsigned int _width, unsigned int _height) { }
+    
+    
+  };
+  
   class ContextWx : public Context {
   public:
-    ContextWx(unsigned int width = 0, unsigned int height = 0) 
-      : image(0), gc(0) {
+    ContextWx(unsigned int _width = 0, unsigned int _height = 0)
+      : Context(width, height),
+      default_surface(_width, _height),
+      gc(0) {
       resize(width, height);      
     }
     ~ContextWx() {
-      if (gc) {
-	wxDELETE(gc);
-      }
-      delete image;
+      // delete gc;
     }
 
     void resize(unsigned int width, unsigned int height) {
       std::cerr << "resizing to " << width << " " << height << std::endl;
-      if (gc) {
-	std::cerr << "deleting gc\n";
-	wxDELETE(gc);
-      }
-      if (image) {
-	std::cerr << "deleting image\n";
-	delete image;      
-      }
+      delete gc;
       if (width && height) {
 	unsigned int pixelCount = width * height;
-	unsigned char * rgbData = new unsigned char[pixelCount * 3];
-	unsigned char * alphaData = new unsigned char[pixelCount];
+	unsigned char * rgbData = (unsigned char *)malloc(pixelCount * 3); // new unsigned char[pixelCount * 3];
+	unsigned char * alphaData = (unsigned char *)malloc(pixelCount); // new unsigned char[pixelCount];
 	memset(rgbData, 0, 3 * pixelCount);
 	memset(alphaData, 0, pixelCount);
-	std::cerr << "creating imagme\n";
-	image = new wxImage(width, height, rgbData, alphaData);
-	gc = wxGraphicsContext::Create(*image);
+	std::cerr << "creating image\n";
+	image = wxImage(wxSize(width, height), rgbData, alphaData);
+	gc = wxGraphicsContext::Create(image);
       } else {
-	gc = wxGraphicsContext::Create(); // dummy context for measuring
+	gc = wxGraphicsContext::Create(); // dummy context for measuring	
       }
-      assert(gc);
       current_path = gc->CreatePath();
       current_font = wxFont(10, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
       std::cerr << "done\n";
@@ -49,8 +46,8 @@ namespace canvas {
     void flush() {
       std::cerr << "flushing\n";
       gc->Flush();
-      wxDELETE(gc);
-      gc = 0;
+      // delete gc;
+      // gc = 0;      
     }
 
     void beginPath() {
@@ -104,7 +101,6 @@ namespace canvas {
 
     Size measureText(const std::string & text) {
       std::cerr << "trying to measure text " << text << std::endl;
-      assert(gc);
       std::cerr << "setting fon\n";
       gc->SetFont(current_font, wxColour(fillStyle.color.red,
 					 fillStyle.color.green,
@@ -129,11 +125,12 @@ namespace canvas {
       gc->FillPath(path);
     }
 
-    wxImage * image;
+    wxImage image;
     wxGraphicsContext * gc;
     
   private:
     wxGraphicsPath current_path;
     wxFont current_font;
+    WxSurface default_surface;
   };
 };
