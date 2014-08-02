9602fb7e25d3d3373977cc60f8b7d6c460df78e0
Mikael Rekola <mikael.rekola@gmail.com>
2014-08-02 17:06:40 +0300

    add constructor with color components

--- src/Color.h
+++ src/Color.h
@@ -8,6 +8,8 @@ namespace canvas {
   public:
     Color() { }
     Color(const std::string & s);
+    Color(unsigned char _red, unsigned char _green, unsigned char _blue)
+      : red(_red), green(_green), blue(_blue) { }
     
     unsigned char red = 0, green = 0, blue = 0;
   };
