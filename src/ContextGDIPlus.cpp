32736cbc54100559ce0b6c32b59f7a19d8ad5b9a
Mikael Rekola <mikael.rekola@gmail.com>
2014-08-02 06:25:30 +0300

    modify GDI+ context to get it working

--- src/ContextGDIPlus.cpp
+++ src/ContextGDIPlus.cpp
@@ -1,2 +1,6 @@
 #include "ContextGDIPlus.h"
 
+bool canvas::GDIPlusSurface::is_initialized = false;
+ULONG_PTR canvas::GDIPlusSurface::m_gdiplusToken;
+
+
--- src/ContextGDIPlus.h
+++ src/ContextGDIPlus.h
@@ -1,7 +1,8 @@
 #include "Context.h"
 
+#undef WIN32_LEAN_AND_MEAN
+#include <Windows.h>
 #include <gdiplus.h>
-// #include <Windows.h>
 
 #include <iostream>
 #include <cassert>
@@ -13,45 +14,59 @@ namespace canvas {
   public:
     friend class ContextGDIPlus;
 
-    GDIPlusSurface(unsigned int _width, unsigned int _height) : Surface(_width, _height) {
+    GDIPlusSurface(unsigned int _width, unsigned int _height) : Surface(_width, _height), bitmap(_width, _height) {
       if (!is_initialized) {
 	is_initialized = true;
 	// Initialize GDI+
 	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
 	Gdiplus::GdiplusStartup(&m_gdiplusToken, &gdiplusStartupInput, NULL);
       }
-
-      dc.CreateCompatibleDC(0);
-      bitmap.CreateCompatibleBitmap(&dc, _width, _height);
-      dc.SelectObject(bitmap);
-      
-      // dc.BitBlt(0, 0, 300, 300, &bufferDC, 0, 0, SRCCOPY);
     }
-    GDIPlusSurface(unsigned int width, unsigned int height, unsigned char * data);
-    ~CairoSurface();
+    GDIPlusSurface(unsigned int _width, unsigned int _height, unsigned char * _data) : Surface(_width, _height),  bitmap(_width, _height) {
 
-    void resize(unsigned int width, unsigned int height);
+    }
+    ~GDIPlusSurface() {
+      delete[] output_data;
+    }
+    void resize(unsigned int width, unsigned int height) {
+    }
+    void flush() {
+      Gdiplus::Rect rect(0, 0, bitmap.GetWidth(), bitmap.GetHeight());
+      Gdiplus::BitmapData data;
+      bitmap.LockBits(&rect, Gdiplus::ImageLockModeRead, PixelFormat32bppARGB, &data);
+      // glPixelStorei(GL_UNPACK_ROW_LENGTH, data.Width);
+      delete[] output_data;
+      size_t s = bitmap.GetWidth() * bitmap.GetHeight() * 4;
+      output_data = new unsigned char[s];
+      memcpy(output_data, data.Scan0, s);
+      bitmap.UnlockBits(&data);
+    }
 
-    unsigned char * getBuffer();
-    const unsigned char * getBuffer() const;
-
-    // cairo_image_surface_get_stride(surface);
-
-    cairo_surface_t * surface;
+    unsigned char * getBuffer() {
+      return output_data;
+    }
+    const unsigned char * getBuffer() const {
+      return output_data;
+    }
 
   protected:
-    CDC dc;
-    CBitmap bitmap;
+    // HDC dc;
+    // HBITMAP bitmap;
+    Gdiplus::Bitmap bitmap;
+    unsigned char * output_data = 0;
+
+    static bool is_initialized;
+    static ULONG_PTR m_gdiplusToken;
   };
 
   class ContextGDIPlus : public Context {
   public:
-    ContextCairo(unsigned int _width = 0, unsigned int _height = 0)
+    ContextGDIPlus(unsigned int _width = 0, unsigned int _height = 0)
       : Context(_width, _height),
 	default_surface(_width, _height),
-	g(default_surface.dc)
+	g(&default_surface.bitmap)
     {
-      g.Clear(GdiPlus::Color::Green);
+      g.Clear(Gdiplus::Color::Green);
     }
     ~ContextGDIPlus() {
       
@@ -81,35 +96,35 @@ namespace canvas {
 
     }
     void arc(double x, double y, double r, double a0, double a1, bool t = false) {
-      current_path.AddArc(x, y, 2 * r, 2 * r, a0, a1 - a0);
+      current_path.AddArc(Gdiplus::REAL(x), Gdiplus::REAL(y), Gdiplus::REAL(2 * r), Gdiplus::REAL(2 * r), Gdiplus::REAL(a0), Gdiplus::REAL(a1 - a0));
     }
     void clearRect(double x, double y, double w, double h) { }
     void moveTo(double x, double y) {
-      current_position = pair<double, double>(x, y);
+      current_position = std::pair<double, double>(x, y);
     }
     void lineTo(double x, double y) {
-      current_path.AddLine(current_position->first, current_position->second, x, y);
-      current_position = pair<double, double>(x, y);
+      current_path.AddLine(Gdiplus::PointF(current_position.first, current_position.second), Gdiplus::PointF(x, y));
+      current_position = std::pair<double, double>(x, y);
     }
     void stroke() {
-      GdiPlus::Pen pen(Color(strokeStyle.color.red, strokeStyle.color.green, strokeStyle.color.blue ));
-      g.DrawPath(pen, current_path);
-      current_path.Clear();  
+      Gdiplus::Pen pen(Gdiplus::Color(strokeStyle.color.red, strokeStyle.color.green, strokeStyle.color.blue ));
+      g.DrawPath(&pen, &current_path);
+//      current_path.Clear();  
     }
     void fill() {
-      GdiPlus::SolidBrush brush(Color(fillStyle.color.red, fillStyle.color.green, fillStyle.color.blue ));
-      g.FillPath(brush, current_path);
-      current_path.Clear();  
+      Gdiplus::SolidBrush brush(Gdiplus::Color(fillStyle.color.red, fillStyle.color.green, fillStyle.color.blue ));
+      g.FillPath(&brush, &current_path);
+//      current_path.Clear();  
     }
     void fillText(const std::string & text, double x, double y) {
       std::wstring text2;
       for (unsigned int i = 0; i < text.size(); i++) {
 	text2 += (wchar_t)text[i];
       }
-      GdiPlus::Font font(&FontFamily(L"Arial"), 12);
+      Gdiplus::Font font(&Gdiplus::FontFamily(L"Arial"), 12);
       // LinearGradientBrush brush(Rect(0,0,100,100), Color::Red, Color::Yellow, LinearGradientModeHorizontal);
-      GdiPlus::Brush brush(Color(fillStyle.color.red, fillStyle.color.green, fillStyle.color.blue ));
-      g.DrawString(text2.data(), text2.size(), &font, PointF(x, y), &brush);
+      Gdiplus::SolidBrush brush(Gdiplus::Color(fillStyle.color.red, fillStyle.color.green, fillStyle.color.blue ));
+      g.DrawString(text2.data(), text2.size(), &font, Gdiplus::PointF(x, y), &brush);
     }
     Size measureText(const std::string & text) {
       return {0,0};
@@ -124,12 +139,9 @@ namespace canvas {
 
   protected:
     GDIPlusSurface default_surface;
-    GdiPlus::Graphics g;
-    GraphicsPath current_path;
+    Gdiplus::Graphics g;
+    Gdiplus::GraphicsPath current_path;
     std::pair<double, double> current_position;
-
-    static bool is_initialized;
-    static ULONG_PTR m_gdiplusToken;    
   private:   
   };
 };
