f149f8a8fbcb06cdc1c7899ea7b9fbbbb4570e59
Mikael Rekola <mikael.rekola@gmail.com>
2014-08-09 03:49:47 +0300

    add slant and weight

--- src/Font.h
+++ src/Font.h
@@ -6,11 +6,35 @@
 namespace canvas {
   class Font {
   public:
+    enum Slant {
+      NORMAL_SLANT = 1,
+      ITALIC,
+      OBLIQUE
+    };
+    enum Weight {
+      NORMAL = 1,
+      BOLD,
+      BOLDER,
+      LIGHTER
+#if 0
+100
+200
+300
+400
+500
+600
+700
+800
+900
+#endif
+    };
     Font() { }
     Font(const std::string & s) { }
 
-    std::string family = "serif";
+    std::string family = "sans-serif";
     float size = 10.0f;
+    Slant slant = NORMAL_SLANT;
+    Weight weight = NORMAL;
   };
 };
 
