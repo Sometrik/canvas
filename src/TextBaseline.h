deced876b74de4e3912484b15dc170278a502a1d
Mikael Rekola <mikael.rekola@gmail.com>
2014-08-10 21:51:47 +0300

    add class TextBaseline

---
+++ src/TextBaseline.h
@@ -0,0 +1,43 @@
+#ifndef _CANVAS_TEXTBASELINE_H_
+#define _CANVAS_TEXTBASELINE_H_
+
+#include <cstring>
+
+namespace canvas {
+  class TextBaseline {
+  public:
+    enum TextBaselineType {
+      TOP = 1,
+      HANGING,
+      MIDDLE,
+      ALPHABETIC,
+      IDEOGRAPHIC,
+      BOTTOM
+    };
+    TextBaseline(TextBaselineType _type = ALPHABETIC) : type(_type) { }
+    TextBaseline(const std::string & _type) { setType(_type.c_str()); }
+    TextBaseline(const char * _type) { setType(_type); }
+    TextBaseline & operator=(const std::string & _type) { setType(_type.c_str()); return *this; }
+    TextBaseline & operator=(const char * _type) { setType(_type); return *this; }
+
+    TextBaselineType getType() const { return type; }
+
+  private:
+    void setType(const char * _type) {
+      if (strcmp(_type, "top") == 0) type = TOP;
+      else if (strcmp(_type, "hanging") == 0) type = HANGING;
+      else if (strcmp(_type, "middle") == 0) type = MIDDLE;
+      else if (strcmp(_type, "alphabetic") == 0) type = ALPHABETIC;
+      else if (strcmp(_type, "ideographic") == 0) type = IDEOGRAPHIC;
+      else if (strcmp(_type, "bottom") == 0) type = BOTTOM;
+      else {
+	assert(0);
+	type = TOP;
+      }
+    };
+  private:
+    TextBaselineType type;
+  };
+};
+
+#endif
