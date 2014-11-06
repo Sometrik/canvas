28cbe798f5aa2240b040791ab59399bdb7a3181b
Mikael Rekola <mikael.rekola@gmail.com>
2014-11-06 02:52:42 +0200

    create TextAlign.h

---
+++ src/TextAlign.h
@@ -0,0 +1,14 @@
+#ifndef _CANVAS_TEXTALIGN_H_
+#define _CANVAS_TEXTALIGN_H_
+
+namespace canvas {
+  enum TextAlign {
+    START = 1,
+    END,
+    LEFT,
+    CENTER,
+    RIGHT
+  };
+};
+
+#endif
