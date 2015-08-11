6485aeef887de7467afe02239be7ea54d6931e26
Mikael Rekola <mikael.rekola@gmail.com>
2015-08-11 13:54:16 +0300

    add ImageFormat

--- src/Image.h
+++ src/Image.h
@@ -4,23 +4,25 @@
 #include <cstring>
 #include <cassert>
 
+#include "ImageFormat.h"
+
 namespace canvas {
   class Image {
   public:
-    Image() : width(0), height(0), data(0), has_alpha(false) { }
-    Image(unsigned int _width, unsigned int _height, const unsigned char * _data, bool _has_alpha)
-      : width(_width), height(_height), has_alpha(_has_alpha)
+  Image() : width(0), height(0), data(0), format(ImageFormat::UNDEF) { }
+   Image(unsigned int _width, unsigned int _height, const unsigned char * _data, const ImageFormat & _format)
+     : width(_width), height(_height), format(_format)
     {
       assert(_data);
-      unsigned int s = width * height * (has_alpha ? 4 : 3);
+      unsigned int s = width * height * format.getBytesPerPixel();
       data = new unsigned char[s];
       memcpy(data, _data, s);
     }
     Image(const Image & other)
-      : width(other.getWidth()), height(other.getHeight()), has_alpha(other.hasAlpha())
+      : width(other.getWidth()), height(other.getHeight()), format(other.format)
     {
       if (other.getData()) {
-	unsigned int s = width * height * (has_alpha ? 4 : 3);
+	size_t s = width * height * format.getBytesPerPixel();
 	data = new unsigned char[s];
 	memcpy(data, other.getData(), s);
       } else {
@@ -36,9 +38,9 @@ namespace canvas {
 	delete[] data;
 	width = other.width;
 	height = other.height;
-	has_alpha = other.has_alpha;
+	format = other.format;
 	if (other.data) {
-	  unsigned int s = width * height * (has_alpha ? 4 : 3);
+	  unsigned int s = width * height * format.getBytesPerPixel();
 	  data = new unsigned char[s];
 	  memcpy(data, other.data, s);
 	} else {
@@ -51,13 +53,13 @@ namespace canvas {
     bool isValid() const { return width != 0 && height != 0; }
     unsigned int getWidth() const { return width; }
     unsigned int getHeight() const { return height; }
-    bool hasAlpha() const { return has_alpha; }
+    const ImageFormat & getFormat() const { return format; }
     const unsigned char * getData() const { return data; }
 
   private:
     unsigned int width, height;
     unsigned char * data;
-    bool has_alpha;
+    ImageFormat format;
   };
 };
 #endif
