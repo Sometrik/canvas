7ea0df719bdf423c2ba1bfb33b3a8dc6fdefdd65
Mikael Rekola <mikael.rekola@gmail.com>
2015-08-19 04:05:27 +0300

    add InternalFormat.h

---
+++ src/InternalFormat.h
@@ -0,0 +1,17 @@
+#ifndef _INTERNALFORMAT_H_
+#define _INTERNALFORMAT_H_
+
+namespace canvas {
+  enum InternalFormat {
+    RG8 = 1,
+    RGB5,
+    RGBA4,
+    RGBA8,
+    COMPRESSED_RG,
+    COMPRESSED_RGB,
+    COMPRESSED_RGBA,
+    LUMINANCE_ALPHA
+  };
+};
+
+#endif
