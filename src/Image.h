de221ff1b121170402a7ecd54d5e14c5cc278bfe
Mikael Rekola <mikael.rekola@gmail.com>
2015-08-22 04:35:20 +0300

    add new constructor and changeFormat method

--- src/Image.h
+++ src/Image.h
@@ -3,6 +3,7 @@
 
 #include <cstring>
 #include <cassert>
+#include <memory>
 
 #include "ImageFormat.h"
 
@@ -17,6 +18,11 @@ namespace canvas {
       unsigned int s = width * height * format.getBytesPerPixel();
       data = new unsigned char[s];
       memcpy(data, _data, s);
+    }
+  Image(unsigned int _width, unsigned int _height, const ImageFormat & _format) : width(_width), height(_height), format(_format) {
+      unsigned int s = width * height * format.getBytesPerPixel();
+      data = new unsigned char[s];
+      memset(data, 0, s);
     }
     Image(const Image & other)
       : width(other.getWidth()), height(other.getHeight()), format(other.format)
@@ -50,15 +56,19 @@ namespace canvas {
       return *this;
     }
 
+    std::shared_ptr<Image> changeFormat(const ImageFormat & target_format) const;
+
     bool isValid() const { return width != 0 && height != 0; }
     unsigned int getWidth() const { return width; }
     unsigned int getHeight() const { return height; }
     const ImageFormat & getFormat() const { return format; }
     const unsigned char * getData() const { return data; }
 
+  protected:
+    
   private:
     unsigned int width, height;
-    unsigned char * data;
+    unsigned char * data = 0;
     ImageFormat format;
   };
 };
