26ab902265a605cbd78f22b017f3c1a44023db83
Mikael Rekola <mikael.rekola@gmail.com>
2015-08-24 17:49:43 +0300

    change component orders and temporarily disable green and blue from luminance

--- src/Image.cpp
+++ src/Image.cpp
@@ -23,9 +23,9 @@ Image::changeFormat(const ImageFormat & target_format) const {
       int green = RGBA_TO_GREEN(v);
       int blue = RGBA_TO_BLUE(v);
       int alpha = RGBA_TO_ALPHA(v);
-      int lum = (red + green + blue) / 3;
-      if (lum >= 255) lum = 255;
-      *output_data++ = (lum << 8) | alpha;
+      // int lum = (red + green + blue) / 3;
+      // if (lum >= 255) lum = 255;
+      *output_data++ = (alpha << 8) | red;
     }
   } else {
     for (unsigned int i = 0; i < n; i++) {
@@ -34,7 +34,7 @@ Image::changeFormat(const ImageFormat & target_format) const {
       int green = RGBA_TO_GREEN(v) >> 2;
       int blue = RGBA_TO_BLUE(v) >> 3;
       
-      *output_data++ = PACK_RGB565(red, green, blue);
+      *output_data++ = PACK_RGB565(blue, green, red);
     }
   }
 
