bcf029a17f1aa4dcc90992b6ef924e12243683f2
Mikael Rekola <mikael.rekola@gmail.com>
2015-08-24 16:04:47 +0300

    add luminosity/alpha format to changFormat

--- src/Image.cpp
+++ src/Image.cpp
@@ -16,14 +16,29 @@ Image::changeFormat(const ImageFormat & target_format) const {
   unsigned short * output_data = (unsigned short *)tmp;
   const unsigned int * input_data = (const unsigned int *)data;
 
-  for (unsigned int i = 0; i < n; i++) {
-    int v = input_data[i];
-    int red = RGBA_TO_RED(v) >> 3;
-    int green = RGBA_TO_GREEN(v) >> 2;
-    int blue = RGBA_TO_BLUE(v) >> 3;
-    
-    *output_data++ = PACK_RGB565(red, green, blue);
+  if (target_format.getNumChannels() == 2) {
+    for (unsigned int i = 0; i < n; i++) {
+      int v = input_data[i];
+      int red = RGBA_TO_RED(v);
+      int green = RGBA_TO_GREEN(v);
+      int blue = RGBA_TO_BLUE(v);
+      int alpha = RGBA_TO_ALPHA(v);
+      int lum = (red + green + blue) / 3;
+      if (lum >= 255) lum = 255;
+      *output_data++ = (lum << 8) | alpha;
+    }
+  } else {
+    for (unsigned int i = 0; i < n; i++) {
+      int v = input_data[i];
+      int red = RGBA_TO_RED(v) >> 3;
+      int green = RGBA_TO_GREEN(v) >> 2;
+      int blue = RGBA_TO_BLUE(v) >> 3;
+      
+      *output_data++ = PACK_RGB565(red, green, blue);
+    }
   }
 
-  return std::shared_ptr<Image>(new Image(getWidth(), getHeight(), tmp, target_format));
+  auto r = std::shared_ptr<Image>(new Image(getWidth(), getHeight(), tmp, target_format));
+  delete tmp;
+  return r;
 }
