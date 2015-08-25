b447053211cca133f8809b2c671f3130ee5bd54c
Mikael Rekola <mikael.rekola@gmail.com>
2015-08-25 16:43:59 +0300

    add Operator to renderText

--- src/Surface.h
+++ src/Surface.h
@@ -64,8 +64,9 @@ namespace canvas {
     }
 
     virtual void clip(const Path & path, float display_scale) = 0;
+    virtual void resetClip() = 0;
     virtual void renderPath(RenderMode mode, const Path & path, const Style & style, float lineWidth, Operator op, float display_scale) = 0;
-    virtual void renderText(RenderMode mode, const Font & font, const Style & style, TextBaseline textBaseline, TextAlign textAlign, const std::string & text, double x, double y, float lineWidth, float display_scale) = 0;
+    virtual void renderText(RenderMode mode, const Font & font, const Style & style, TextBaseline textBaseline, TextAlign textAlign, const std::string & text, double x, double y, float lineWidth, Operator op, float display_scale) = 0;
     virtual TextMetrics measureText(const Font & font, const std::string & text, float display_scale) = 0;
 	  
     virtual void drawImage(Surface & _img, double x, double y, double w, double h, float alpha = 1.0f, bool imageSmoothingEnabled = true) = 0;
