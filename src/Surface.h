d2851802f61637dbd97686db39ffddd88a231613
Mikael Rekola <mikael.rekola@gmail.com>
2015-12-03 17:33:37 +0200

    add shadow parameters to drawImage and renderText

--- src/Surface.h
+++ src/Surface.h
@@ -61,11 +61,11 @@ namespace canvas {
     virtual void clip(const Path & path, float display_scale) = 0;
     virtual void resetClip() = 0;
     virtual void renderPath(RenderMode mode, const Path & path, const Style & style, float lineWidth, Operator op, float display_scale, float globalAlpha, float shadowBlur, float shadowOffsetX, float shadowOffsetY, const Color & shadowColor) = 0;
-    virtual void renderText(RenderMode mode, const Font & font, const Style & style, TextBaseline textBaseline, TextAlign textAlign, const std::string & text, double x, double y, float lineWidth, Operator op, float display_scale, float alpha) = 0;
+    virtual void renderText(RenderMode mode, const Font & font, const Style & style, TextBaseline textBaseline, TextAlign textAlign, const std::string & text, double x, double y, float lineWidth, Operator op, float display_scale, float globalAlpha, float shadowBlur, float shadowOffsetX, float shadowOffsetY, const Color & shadowColor) = 0;
     virtual TextMetrics measureText(const Font & font, const std::string & text, float display_scale) = 0;
 	  
-    virtual void drawImage(Surface & _img, double x, double y, double w, double h, float alpha = 1.0f, bool imageSmoothingEnabled = true) = 0;
-    virtual void drawImage(const Image & _img, double x, double y, double w, double h, float alpha = 1.0f, bool imageSmoothingEnabled = true) = 0;
+    virtual void drawImage(Surface & _img, double x, double y, double w, double h, float globalAlpha, float shadowBlur, float shadowOffsetX, float shadowOffsetY, const Color & shadowColor, bool imageSmoothingEnabled = true) = 0;
+    virtual void drawImage(const Image & _img, double x, double y, double w, double h, float globalAlpha, float shadowBlur, float shadowOffsetX, float shadowOffsetY, const Color & shadowColor, bool imageSmoothingEnabled = true) = 0;
     virtual void save() = 0;
     virtual void restore() = 0;
     
