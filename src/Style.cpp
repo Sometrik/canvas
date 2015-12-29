dc686b4f9e78d283c84c069aa1d25e46889451a3
Mikael Rekola <mikael.rekola@gmail.com>
2015-12-29 07:15:30 +0200

    add include from GraphicsState.h, add function call operators

--- src/Style.cpp
+++ src/Style.cpp
@@ -1,9 +1,13 @@
 #include <Style.h>
 
+#include <GraphicsState.h>
+
 using namespace canvas;
 
+#if 0
 Style::Style(const std::string & s) : color(s) { }
 Style::Style(const Color & _color) : color(_color) { }
+#endif
 
 Style &
 Style::operator=(const std::string & s) {
@@ -18,3 +22,18 @@ Style::operator=(const Color & _color) {
   type = SOLID;
   return *this;
 }
+
+GraphicsState &
+Style::operator()(const std::string & s) {
+  color = s;
+  type = SOLID;
+  return *context;
+}
+					
+GraphicsState &
+Style::operator()(const Color & _color) {
+  color = _color;
+  type = SOLID;
+  return *context;
+}
+					
