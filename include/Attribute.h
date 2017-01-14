3fa62f56ef5d605b8d8af8ed2d76ba785454773a
Mikael Rekola <mikael.rekola@gmail.com>
2017-01-14 03:00:54 +0200

    finish template based implementation

--- include/Attribute.h
+++ include/Attribute.h
@@ -5,12 +5,18 @@
 
 namespace canvas {
   class GraphicsState;
-  
+
+  inline void assignValue(float & a, const std::string & b) {
+    a = atof(b.c_str());
+  }
+  inline void assignValue(bool & a, const std::string & b) {
+    a = !b.empty() && b != "0" && b != "false";
+  }
+
   class AttributeBase {
   public:
     AttributeBase(GraphicsState * _context) : context(_context) { }
-    AttributeBase(const Attribute & other) = delete;
-    virtual ~AttributeBase() { }
+    AttributeBase(const AttributeBase & other) : context(other.context) { }
 
   protected:
     GraphicsState & getContext() { return *context; }
@@ -22,14 +28,30 @@ namespace canvas {
   template<class T>
   class Attribute : public AttributeBase {
   public:
-    Attribute(GraphicsState * _context) : value() { }
-    Attribute(GraphicsState * _context, T _value) : Attribute(_context), value(_value) { }
-    Attribute(const Attribute<T> & other) : Attribute(other._context), value(other.value) { }
+    Attribute(GraphicsState * _context) : AttributeBase(_context), value() { }
+    Attribute(GraphicsState * _context, T _value) : AttributeBase(_context), value(_value) { }
+    Attribute(const Attribute<T> & other) : AttributeBase(other), value(other.value) { }
   
-    Attribute<T> & operator=(const Attribute<T> & other) { value = other.value; return *this; }
-    Attribute<T> & operator=(T _value) { value = _value; return *this; }
-
-    GraphicsState & operator()(T _value) { value = _value; return getContext(); }
+    Attribute<T> & operator=(const Attribute<T> & other) {
+      value = other.value;
+      return *this;
+    }
+    Attribute<T> & operator=(T _value) {
+      value = _value;
+      return *this;
+    }
+    Attribute<T> & operator=(const std::string & s) {
+      assignValue(value, s);
+      return *this;
+    }
+    GraphicsState & operator()(T _value) {
+      value = _value;
+      return getContext();
+    }
+    GraphicsState & operator()(const std::string & s) {
+      assignValue(value, s);
+      return getContext();
+    }
 
     T get() const { return value; }
     
