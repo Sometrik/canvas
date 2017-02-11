560281bdc18942c5a2b4c905d6de7993b4260b22
Mikael Rekola <mikael.rekola@gmail.com>
2017-02-11 15:39:47 +0200

    add virtual destructor

--- include/FilenameConverter.h
+++ include/FilenameConverter.h
@@ -4,6 +4,7 @@
 namespace canvas {
   class FilenameConverter {
   public:
+    virtual ~FilenameConverter() = default;
     virtual bool convert(const std::string & input, std::string & output) = 0;
   };
 };
