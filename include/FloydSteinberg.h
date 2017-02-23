03a73cb285955974eac0ac38f91a14a93cc0a604
Mikael Rekola <mikael.rekola@gmail.com>
2017-02-23 21:17:34 +0200

    create base class for ditherer

--- include/FloydSteinberg.h
+++ include/FloydSteinberg.h
@@ -1,36 +1,15 @@
 #ifndef _FLOYDSTEINBERG_H_
 #define _FLOYDSTEINBERG_H_
 
-#include <ImageData.h>
+#include <Ditherer.h>
 
 namespace canvas {
-  class FloydSteinberg {
+  class FloydSteinberg : public Ditherer {
   public:
-    FloydSteinberg(const ImageData * _image, InternalFormat target_format);
+    FloydSteinberg(const ImageData & input_image, InternalFormat _target_format)
+      : Ditherer(input_image, _target_format) { }
 
-    std::unique_ptr<ImageData> apply();
-    
-  private:
-    inline void addError(unsigned int x, unsigned int y, float weight, unsigned char r_error, unsigned char g_error, unsigned char b_error, unsigned char a_error) {
-      unsigned int offset = 4 * (y * width + x);
-      int r = int(input_data[offset++] + weight * r_error);
-      int g = int(input_data[offset++] + weight * g_error);
-      int b = int(input_data[offset++] + weight * b_error);
-      int a = int(input_data[offset++] + weight * a_error);
-      if (r > 0xff) r = 0xff;
-      if (g > 0xff) g = 0xff;
-      if (b > 0xff) b = 0xff;
-      if (a > 0xff) a = 0xff;
-      offset -= 4;
-      input_data[offset++] = r;
-      input_data[offset++] = g;
-      input_data[offset++] = b;
-      input_data[offset++] = a;
-    }
-    
-    std::unique_ptr<unsigned char[]> input_data;
-    unsigned int width, height;
-    InternalFormat target_format;
+    std::unique_ptr<ImageData> apply() override;
   };
 };
 
--- src/FloydSteinberg.cpp
+++ src/FloydSteinberg.cpp
@@ -1,33 +1,11 @@
 #include "FloydSteinberg.h"
 
+#include <ImageData.h>
+
 #include <cassert>
 
 using namespace std;
 using namespace canvas;
-
-FloydSteinberg::FloydSteinberg(const ImageData * input_image, InternalFormat _target_format)
-  : target_format(_target_format) {
-  width = input_image->getWidth();
-  height = input_image->getHeight();
-  input_data = std::unique_ptr<unsigned char[]>(new unsigned char[width * height * 4]);
-  auto tmp = input_data.get();
-
-  auto data = input_image->getData();
-  
-  auto & fd = input_image->getImageFormat();
-  unsigned int n = width * height;
-  for (unsigned int i = 0; i < n; i++) {
-    unsigned int input_offset = i * fd.getBytesPerPixel();
-    unsigned char r = data[input_offset++];
-    unsigned char g = fd.getBytesPerPixel() >= 1 ? data[input_offset++] : r;
-    unsigned char b = fd.getBytesPerPixel() >= 2 ? data[input_offset++] : g;
-    unsigned char a = fd.getBytesPerPixel() >= 3 ? data[input_offset++] : 0xff;
-    *tmp++ = r;
-    *tmp++ = g;
-    *tmp++ = b;
-    *tmp++ = a;
-  }
-}
   
 unique_ptr<ImageData>
 FloydSteinberg::apply() {  
