6e6413cef6bc3679e95aed5aa1dfaea1ed29e4ed
Mikael Rekola <mikael.rekola@gmail.com>
2017-10-29 14:17:35 +0200

    remove Context.cpp. Fixes #69

--- include/Context.h
+++ include/Context.h
@@ -27,23 +27,58 @@ namespace canvas {
     virtual ~Context() { }
 
     virtual std::unique_ptr<Surface> createSurface(const ImageData & image) = 0;
+
     virtual std::unique_ptr<Surface> createSurface(unsigned int _width, unsigned int _height, unsigned int _num_channels) = 0;
-    virtual void resize(unsigned int _width, unsigned int _height);
+
+    virtual void resize(unsigned int _width, unsigned int _height) {
+      getDefaultSurface().resize(_width, _height, (unsigned int)(_width * getDisplayScale()), (unsigned int)(_height * getDisplayScale()), getDefaultSurface().getNumChannels());
+      hit_regions.clear();
+    }
         
     Context & stroke() { return renderPath(STROKE, currentPath, strokeStyle); }
     Context & stroke(const Path2D & path) { return renderPath(STROKE, path, strokeStyle); }
     Context & fill() { return renderPath(FILL, currentPath, fillStyle); }
     Context & fill(const Path2D & path) { return renderPath(FILL, path, fillStyle); }
-    Context & save();
-    Context & restore();
+    Context & save() {
+      restore_stack.push_back(*this);
+      return *this;
+    }
+    Context & restore() {
+      if (!restore_stack.empty()) {
+	*this = restore_stack.back();
+	restore_stack.pop_back();    
+      }
+      return *this;
+    }
     
     bool isPointInPath(const Path2D & path, double x, double y) { return false; }
     
-    TextMetrics measureText(const std::string & text);
+    TextMetrics measureText(const std::string & text) {
+      return getDefaultSurface().measureText(font, text, textBaseline.get(), getDisplayScale());
+    }
     
-    Context & fillRect(double x, double y, double w, double h);
-    Context & strokeRect(double x, double y, double w, double h);
-    Context & clearRect(double x, double y, double w, double h);
+    Context & fillRect(double x, double y, double w, double h) {
+      beginPath().rect(x, y, w, h);
+      return fill();
+    }
+    
+    Context & strokeRect(double x, double y, double w, double h) {
+      beginPath().rect(x, y, w, h);
+      return stroke();
+    }
+    
+    Context & clearRect(double x, double y, double w, double h) {
+      Path2D path;
+      path.moveTo(currentTransform.multiply(x, y));
+      path.lineTo(currentTransform.multiply(x + w, y));
+      path.lineTo(currentTransform.multiply(x + w, y + h));
+      path.lineTo(currentTransform.multiply(x, y + h));
+      path.closePath();
+      Style style(this);
+      style = Color(0.0f, 0.0f, 0.0f, 0.0f);
+      return renderPath(FILL, path, style, COPY);
+    }
+    
     Context & fillText(const std::string & text, double x, double y) { return renderText(FILL, fillStyle, text, currentTransform.multiply(x, y)); }
     Context & strokeText(const std::string & text, double x, double y) { return renderText(STROKE, strokeStyle, text, currentTransform.multiply(x, y)); }
     
@@ -58,11 +93,51 @@ namespace canvas {
     Context & drawImage(Context & other, double x, double y, double w, double h) {
       return drawImage(other.getDefaultSurface(), x, y, w, h);
     }
+    
     Context & drawImage(Image & img, double x, double y, double w, double h) {
       return drawImage(img.getData(), x, y, w, h);
     }
-    virtual Context & drawImage(const ImageData & img, double x, double y, double w, double h);
-    virtual Context & drawImage(Surface & img, double x, double y, double w, double h);    
+    
+    virtual Context & drawImage(const ImageData & img, double x, double y, double w, double h) {
+      Point p = currentTransform.multiply(x, y);
+      if (hasNativeShadows()) {
+	getDefaultSurface().drawImage(img, p, w, h, getDisplayScale(), globalAlpha.get(), shadowBlur.get(), shadowOffsetX.get(), shadowOffsetY.get(), shadowColor.get(), clipPath, imageSmoothingEnabled.get());
+      } else {
+	if (hasShadow()) {
+	  float b = shadowBlur.get(), bs = shadowBlur.get() * getDisplayScale();
+	  float bi = int(ceil(b));
+	  auto shadow = createSurface(getDefaultSurface().getLogicalWidth() + 2 * bi, getDefaultSurface().getLogicalHeight() + 2 * bi, R8);
+	  shadow->drawImage(img, Point(x + b + shadowOffsetX.get(), y + b + shadowOffsetY.get()), w, h, getDisplayScale(), globalAlpha.get(), 0.0f, 0.0f, 0.0f, shadowColor.get(), clipPath, imageSmoothingEnabled.get());
+	  // shadow->colorFill(shadowColor.get());
+	  auto shadow1 = shadow->blur(bs, bs);
+	  auto shadow2 = shadow1->colorize(shadowColor.get());
+	  getDefaultSurface().drawImage(*shadow2, Point(-b, -b), shadow->getLogicalWidth(), shadow->getLogicalHeight(), getDisplayScale(), 1.0f, 0.0f, 0.0f, 0.0f, shadowColor.get(), Path2D(), false);
+	}
+	getDefaultSurface().drawImage(img, p, w, h, getDisplayScale(), globalAlpha.get(), 0.0f, 0.0f, 0.0f, shadowColor.get(), clipPath, imageSmoothingEnabled.get());
+      }
+      return *this;
+    }
+    
+    virtual Context & drawImage(Surface & img, double x, double y, double w, double h) {
+      Point p = currentTransform.multiply(x, y);
+      if (hasNativeShadows()) {
+	getDefaultSurface().drawImage(img, p, w, h, getDisplayScale(), globalAlpha.get(), shadowBlur.get(), shadowOffsetX.get(), shadowOffsetY.get(), shadowColor.get(), clipPath, imageSmoothingEnabled.get());
+      } else {
+	if (hasShadow()) {
+	  float b = shadowBlur.get(), bs = shadowBlur.get() * getDisplayScale();
+	  float bi = int(ceil(b));
+	  auto shadow = createSurface(getDefaultSurface().getLogicalWidth() + 2 * bi, getDefaultSurface().getLogicalHeight() + 2 * bi, R8);
+	  
+	  shadow->drawImage(img, Point(p.x + b + shadowOffsetX.get(), p.y + b + shadowOffsetY.get()), w, h, getDisplayScale(), globalAlpha.get(), 0.0f, 0.0f, 0.0f, shadowColor.get(), clipPath, imageSmoothingEnabled.get());
+	  // shadow->colorFill(shadowColor.get());
+	  auto shadow1 = shadow->blur(bs, bs);
+	  auto shadow2 = shadow1->colorize(shadowColor.get());
+	  getDefaultSurface().drawImage(*shadow2, Point(-b, -b), shadow->getLogicalWidth(), shadow->getLogicalHeight(), getDisplayScale(), 1.0f, 0.0f, 0.0f, 0.0f, shadowColor.get(), Path2D(), false);
+	}
+	getDefaultSurface().drawImage(img, p, w, h, getDisplayScale(), globalAlpha.get(), 0.0f, 0.0f, 0.0f, shadowColor.get(), clipPath, imageSmoothingEnabled.get());
+      }
+      return *this;
+    }
         
     Style & createLinearGradient(double x0, double y0, double x1, double y1) {
       current_linear_gradient.setType(Style::LINEAR_GRADIENT);
@@ -95,8 +170,51 @@ namespace canvas {
 #endif
     
   protected:
-    Context & renderPath(RenderMode mode, const Path2D & path, const Style & style, Operator op = SOURCE_OVER);
-    Context & renderText(RenderMode mode, const Style & style, const std::string & text, const Point & p, Operator op = SOURCE_OVER);
+    Context & renderPath(RenderMode mode, const Path2D & path, const Style & style, Operator op = SOURCE_OVER) {
+      if (hasNativeShadows()) {
+	getDefaultSurface().renderPath(mode, path, style, lineWidth.get(), op, getDisplayScale(), globalAlpha.get(), shadowBlur.get(), shadowOffsetX.get(), shadowOffsetY.get(), shadowColor.get(), clipPath);
+      } else {
+	if (hasShadow()) {
+	  float b = shadowBlur.get(), bs = shadowBlur.get() * getDisplayScale();
+	  float bi = int(ceil(b));
+	  auto shadow = createSurface(getDefaultSurface().getLogicalWidth() + 2 * bi, getDefaultSurface().getLogicalHeight() + 2 * bi, R8);
+	  Style shadow_style(this);
+	  shadow_style = shadowColor.get();
+	  Path2D tmp_path = path, tmp_clipPath = clipPath;
+	  tmp_path.offset(shadowOffsetX.get() + bi, shadowOffsetY.get() + bi);
+	  tmp_clipPath.offset(shadowOffsetX.get() + bi, shadowOffsetY.get() + bi);
+	  
+	  shadow->renderPath(mode, tmp_path, shadow_style, lineWidth.get(), op, getDisplayScale(), globalAlpha.get(), 0, 0, 0, shadowColor.get(), tmp_clipPath);
+	  auto shadow1 = shadow->blur(bs, bs);
+	  auto shadow2 = shadow1->colorize(shadowColor.get());
+	  getDefaultSurface().drawImage(*shadow2, Point(-b, -b), shadow->getLogicalWidth(), shadow->getLogicalHeight(), getDisplayScale(), 1.0f, 0.0f, 0.0f, 0.0f, shadowColor.get(), Path2D(), false);
+	}
+	getDefaultSurface().renderPath(mode, path, style, lineWidth.get(), op, getDisplayScale(), globalAlpha.get(), 0, 0, 0, shadowColor.get(), clipPath);
+      }
+      return *this;
+    }
+    
+    Context & renderText(RenderMode mode, const Style & style, const std::string & text, const Point & p, Operator op = SOURCE_OVER) {
+      if (hasNativeShadows()) {
+	getDefaultSurface().renderText(mode, font, style, textBaseline.get(), textAlign.get(), text, p, lineWidth.get(), op, getDisplayScale(), globalAlpha.get(), shadowBlur.get(), shadowOffsetX.get(), shadowOffsetY.get(), shadowColor.get(), clipPath);
+      } else {
+	if (hasShadow()) {
+	  float b = shadowBlur.get(), bs = shadowBlur.get() * getDisplayScale();
+	  int bi = int(ceil(b));
+	  auto shadow = createSurface(getDefaultSurface().getLogicalWidth() + 2 * bi, getDefaultSurface().getLogicalHeight() + 2 * bi, R8);
+	  
+	  Style shadow_style(this);
+	  shadow_style = shadowColor.get();
+	  shadow_style.color.alpha = 1.0f;
+	  shadow->renderText(mode, font, shadow_style, textBaseline.get(), textAlign.get(), text, Point(p.x + shadowOffsetX.get() + b, p.y + shadowOffsetY.get() + b), lineWidth.get(), op, getDisplayScale(), globalAlpha.get(), 0.0f, 0.0f, 0.0f, shadowColor.get(), clipPath);
+	  auto shadow1 = shadow->blur(bs, bs);
+	  auto shadow2 = shadow1->colorize(shadowColor.get());
+	  getDefaultSurface().drawImage(*shadow2, Point(-b, -b), shadow->getLogicalWidth(), shadow->getLogicalHeight(), getDisplayScale(), 1.0f, 0.0f, 0.0f, 0.0f, shadowColor.get(), Path2D(), false);
+	}
+	getDefaultSurface().renderText(mode, font, style, textBaseline.get(), textAlign.get(), text, p, lineWidth.get(), op, getDisplayScale(), globalAlpha.get(), 0.0f, 0.0f, 0.0f, shadowColor.get(), clipPath);
+      }
+      return *this;
+    }
     virtual bool hasNativeShadows() const { return false; }
     virtual bool hasNativeEmoticons() const { return false; }
 
